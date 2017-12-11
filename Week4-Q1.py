fa7a1142d45ab1aacfff52e0f0faa874d5c89923
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2017-12-11 14:28:44 +0000

    Assignment 2 - Week 4 Answer Q1

---
+++ Week4-Q1.py
@@ -0,0 +1,6 @@
+#Week 4 - Question 1-Explain the mistake in the following code: 
+radius = int(input("Enter the radius of your circle: ")) 
+x = 3.14 
+pi = x 
+area = pi * (radius ** 2) 
+print ("The area of a circle is: %.2f" %area) 
