687cd00fde04b725c40a78040c5348b0cd010f55
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2017-12-11 14:54:44 +0000

    Assignment 2 - Week 4 Answer Q10

---
+++ Week4-Q10.py
@@ -0,0 +1,7 @@
+#Week 4 - Question 10
+p=17
+q=18
+print("The answer   'i.' is :", p // 10 + p % 10)
+print(" \nThe answer  'ii.' is :", p % 2  + q % 2)
+print(" \nThe answer 'iii.' is :", (p + q) // 2)
+print(" \nThe answer  'iv.' is :", (p + q) / 2.0)
