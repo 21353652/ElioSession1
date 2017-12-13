58b01cf30eb252dac586893d268480da24756423
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2017-12-13 19:54:45 +0000

    Assignment 2 - Week 5 Answer Q6

---
+++ Week 5 - Q6.py
@@ -0,0 +1,9 @@
+#Week 5 - Question 6
+intnum = int(input("Enter an integer number \t: "))
+remainder = intnum%7
+print("The remainder of the number",intnum," using % 7 is : ",remainder)
+
+divnum = int(intnum/7)
+print("The number 7 'fits' into the number that the user entered :",divnum)
+
+      
