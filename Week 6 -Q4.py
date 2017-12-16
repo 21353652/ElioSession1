b3a4bbe118da5d381eddf1d190fe8dc3a5f83031
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2017-12-16 16:18:44 +0000

    Assignment 2 - Week 6 Answer Q4

---
+++ Week 6 -Q4.py
@@ -0,0 +1,11 @@
+#WEEK 6 - QUESTION 4
+richer = float( input ( " Enter a magnitude on the Richer scale : "))
+if richer >= 8:
+    print ( " Most structures fail !! " )
+elif richer >= 7:
+    print ( " Many buildings destroyed !! " )
+elif richer >= 4.5:
+    print ( " Damage to poorly constructed buildings " )
+else:
+    print ( " No destruction of buildings " )
+    
