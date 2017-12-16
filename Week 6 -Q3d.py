df71aecd6f3b996c96a98b7cc1e5b9d1f6b74b2d
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2017-12-16 16:17:32 +0000

    Assignment 2 - Week 6 Answer Q3d

---
+++ Week 6 -Q3d.py
@@ -0,0 +1,16 @@
+#WEEK 6 - QUESTION 3 D
+grade = int ( input ( " Enter the grade : " ))
+if grade >= 90 :
+    letterGrade = "A"
+elif grade >= 80 :
+    letterGrade = "B"
+elif grade >= 70 :
+    letterGrade = "C"
+elif grade >= 60 :
+    letterGrade = "D"
+else:
+    letterGrade = "F"
+print ( " Your result of grade is : ",letterGrade)
+    
+
+    
