6b1bf3f961933af16c7cc16aea692b05ed2e4728
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2018-01-02 10:46:42 +0000

    Assignment 2 - Week 8 Answer Q1

---
+++ Week 8 -Q1.py
@@ -0,0 +1,8 @@
+#Week 8 - Question 1
+def mystery(x, y) : 
+          result = (x + y) / (y - x) 
+          return result
+def main():
+    print(" What is the result of the call mystery(2, 3)?" ,mystery(2,3))
+        
+main()
