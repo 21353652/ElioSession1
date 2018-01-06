b7d41b843336bcf935f99dee0b870783b33852cf
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2018-01-06 18:46:22 +0000

    Assignment 2 - Week 8 Answer Q6

---
+++ Week 8 -Q6.py
@@ -0,0 +1,13 @@
+#WEEK 8 - Question 8
+import math
+def f(x) : 
+    return g(x) + math.sqrt(h(x)) 
+def g(x) : 
+    return 4 * h(x) 
+def h(x) : 
+    return x * x + k(x) - 1 
+def k(x) : 
+    return 2 * (x + 1)
+def main():
+    print(f(2))
+main()    
