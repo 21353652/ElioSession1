ade6205d5d01c38d7de99a5211828d58ff73ec22
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2018-01-14 16:41:26 +0000

    Assignment 2 - Week 7 Answer Q5 correct

--- Week 7 -Q5.py
+++ Week 7 -Q5.py
@@ -1,18 +1,21 @@
 #WEEK 7 - QUESTION 5
-first = str(input (" Enter the first sentence : "))
-second = str(input (" Enter the second sentence : "))
-long = (first + " " + second )
-print ( long )
-sentence = long.split ( " ")
-#for i in range ( 0, len( long )):
-#    sentence.append ( long[i] )
-print ( sentence )
-sentence.sort ()
-print( len ( sentence ))
+s1 = input ( " Enter the first sentence : ")
+s2 = input ( " Enter the second sentence : ")
+sentence =s1+" "+s2
+print ("\t-------------------------")
+print (" This is one long sentence concatenated with the two sentences given:\n\t", sentence )
+print ("\t-------------------------")
+sentence2 = sentence.split()
+print (" Split the sentence into a  list of words :\n\t",sentence2)
+print ("\t-------------------------")
+print (" Sort the words in alphabetical order and print them out :\n\t",sorted(sentence2))
+print ("\t-------------------------")
+print (" Print the total number of words contained in your list :\n\t",len(sentence2))
+print ("\t-------------------------")
 dictionary = {}
-for word in sentence:
-    dictionary [word] = sentence.count ( word )
-print ( dictionary )
+for word in sentence2:
+    dictionary[word] = sentence2.count(word)
+print (" Create a dictionary :\n\t",dictionary)
 
-    
-    
+
+
