b45445e1d0b33a3018f6a4f55c55ac4bf30d8b08
ELIO INCONTRADA <32064450+21353652@users.noreply.github.com>
2018-01-14 20:04:04 +0000

    Assignment 2 - Week 10 Answer Q3 Class

---
+++ Q3CashRegister.py
@@ -0,0 +1,31 @@
+#WEEK 12 - QUESTION 3
+
+class CashRegister : 
+    
+   def __init__(self) : 
+      self._itemCount = 0 
+      self._totalPrice = 0.0 
+      self._pay = 0.0 
+    
+   def addItem(self, price) : 
+      self._itemCount = self._itemCount + 1 
+      self._totalPrice = self._totalPrice + price  
+       
+    
+   def getTotal(self) : 
+      return self._totalPrice 
+  
+   def getCount(self) : 
+      return self._itemCount 
+  
+   def clear(self) : 
+      self._itemCount = 0 
+      self._totalPrice = 0.0
+
+
+   def givechange(self,paid):
+      return ( paid - self._totalPrice)
+
+
+
+   
